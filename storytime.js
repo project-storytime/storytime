9e8b08f4e3fbb63af86e5c431d54a19b6911250a
kaiete <74324344+kaiete@users.noreply.github.com>
2020-12-23 17:49:12 +0000

    Update and rename storytime.js to sitemap.html

--- storytime.js
+++
@@ -1,2 +0,0 @@
-alert("To use StoryTime, open the console (use ctrl+shift+I on Windows or Linux, and cmd+shift+I on MacOS)")
-console.log("OK, you've opened the console. Now just select a story by using storytime("story","<insert story here>"). Try storytime("story","The Mouse called John")")
